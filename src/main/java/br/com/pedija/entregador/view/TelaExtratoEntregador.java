ed2ebd7c9fddae963204d30a5baeb0dc197f960a
Álef Alves <alefdavialves@gmail.com>
2025-10-24 14:58:51 -0300

    Update TelaExtratoEntregador.java

--- src/main/java/br/com/pedija/entregador/view/TelaExtratoEntregador.java
+++ src/main/java/br/com/pedija/entregador/view/TelaExtratoEntregador.java
@@ -4,7 +4,7 @@ public class TelaExtratoEntregador {
 
     public void extrato() {
 
-        double ganhoSemanal = 1300;
+        double ganhoSemanal = 10300;
         System.out.println("Ganho da semana: " + ganhoSemanal);
 
         double ganhoDiario = 100;
