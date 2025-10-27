cd07e55bc0d76f0230ffd4f92c386a0b69c9c492
Álef Alves <alefdavialves@gmail.com>
2025-10-27 16:24:25 -0300

    .

--- src/main/java/br/com/pedija/entregador/view/TelaExtratoEntregador.java
+++ src/main/java/br/com/pedija/entregador/view/TelaExtratoEntregador.java
@@ -1,14 +1,27 @@
 package br.com.pedija.entregador.view;
 
+import java.util.Scanner;
+
 public class TelaExtratoEntregador {
 
     public void extrato() {
 
-        double ganhoSemanal = 10300;
+        TelaInicialEntregador tela = new TelaInicialEntregador();
+
+        double ganhoSemanal = 1300;
         System.out.println("Ganho da semana: " + ganhoSemanal);
 
         double ganhoDiario = 100;
         System.out.println("Ganho da semana: " + ganhoDiario);
+
+        System.out.println("Voltar (1 - Sim)");
+        Scanner sc = new Scanner(System.in);
+        int voltar = sc.nextInt();
+
+        if (voltar == 1) {
+           tela.TelaInicioEntregador();
+
+        }
 
 
     }
