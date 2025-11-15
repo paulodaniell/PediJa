df56a64733ee0ca27be1614d495ac6075a2a193c
Álef Alves <alefdavialves@gmail.com>
2025-11-15 19:23:29 -0300

    Create App.java

---
+++ src/main/java/br/com/pedija/App.java
@@ -0,0 +1,17 @@
+package br.com.pedija;
+
+import br.com.pedija.superadm.view.EscolherAppview;
+
+
+public class App {
+    public static void main(String[] args) {
+
+
+        EscolherAppview firstview = new EscolherAppview();
+
+        firstview.EscolherAppview();
+
+
+
+    }
+}
