cb6a83e8dc3d9916f918fd8d05417adfa12dc82a
Álef Alves <alefdavialves@gmail.com>
2025-11-17 21:31:33 -0300

    Removendo

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ ! -f target/pedija-1.0-SNAPSHOT.jar ]; then
-    echo "Compilando o projeto pela primeira vez..."
-    mvn clean package -q
-fi
-java -jar target/pedija-1.0-SNAPSHOT.jar
